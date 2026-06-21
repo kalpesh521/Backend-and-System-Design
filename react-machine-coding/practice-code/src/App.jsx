ac0c8dc09ec45da19d00888d9e8f828415977998
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2026-06-21 16:15:34 +0530

    stepper

--- react-machine-coding/practice-code/src/App.jsx
+++ react-machine-coding/practice-code/src/App.jsx
@@ -2,13 +2,37 @@ import { useState } from 'react'
 import './App.css'
 import Otp from './components/Otp'
 import ToastContainer from './components/ToastContainer'
-function App() {
-  const [count, setCount] = useState(0)
+import Stepper from './components/Stepper'
+function App() { 
+
+  const steps = [
+    {
+      label: "Personal Info",
+      content: <div>Personal Information Content</div>,
+    },
+    {
+      label: "Account Info",
+      content: <div>Account Info Content</div>,
+    },
+    {
+      label: "Payment",
+      content: <div>Payment Content</div>,
+    },
+    {
+      label: "Confirmation",
+      content: <div>Confirmation Content</div>,
+    },
+    {
+      label: "Review",
+      content: <div>Review Content</div>,
+    },
+  ];
 
   return (
     <>
       {/* <Otp /> */}
-      <ToastContainer />
+      {/* <ToastContainer />   */}
+      <Stepper steps={steps} />
     </>
   )
 }
